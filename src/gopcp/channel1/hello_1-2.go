42bd359ec8652740ba73d6b137fb097391d34c9a
liukai <869896679@qq.com>
2019-08-19 00:30:15 +0800

    更新注释

--- src/gopcp/channel1/hello_1-2.go
+++ src/gopcp/channel1/hello_1-2.go
@@ -10,7 +10,7 @@ func main() {
 	//总结：导入包时，会优先执行被导入包的init方法，变量（非局部变量）首字母大写才是公开的，才能被包外文件引用，否则只能被包内文件引用
 	inputReader := bufio.NewReader(os.Stdin) //带缓冲读取器，从标准输入进行读取
 	fmt.Println("Please input your name:")
-	input, err := inputReader.ReadString('\n') //以换行符结束一次读取，这个参数必须是字符类型，不能是字符串类型，所以是单引号
+	input, err := inputReader.ReadString('\n') //以换行符结束一次读取，这个参数必须是byte类型，不能是字符串类型，所以是单引号
 	if err != nil {
 		fmt.Printf("Found an error:%s\n", err)
 	} else {
