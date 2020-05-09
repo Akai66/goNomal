1c46922ea20accbd84ca1abb70ccec08af9cecff
liukai <869896679@qq.com>
2020-05-09 15:14:40 +0800

    gopl1.3

---
+++ src/goBasic/1.3/dup2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2:%v\n", err)
+				continue
+			}
+			countLines(f, counts)
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
+}
+
+//make返回的是指针，形参counts是原始参数的一份拷贝，原始参数和形参实际是两个不同的指针，但是内部存的值指向同一块内存，所以被调用函数对map底层数据结构的任何修改，调用者函数都可以通过持有的map引用看到
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
